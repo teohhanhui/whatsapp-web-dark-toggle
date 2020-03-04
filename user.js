8952dc4e529db36cd2b319ab5691b8f5e6436fe7
Teoh Han Hui <teohhanhui@gmail.com>
2020-03-04 13:58:34 +0100

    Initial commit

---
+++ user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         WhatsApp Web dark toggle
+// @namespace    https://github.com/teohhanhui/whatsapp-web-dark-toggle
+// @version      0.1
+// @author       Teoh Han Hui <teohhanhui@gmail.com>
+// @description  Force toggle WIP dark theme on WhatsApp Web
+// @updateURL    https://raw.githubusercontent.com/teohhanhui/whatsapp-web-dark-toggle/master/user.js
+// @downloadURL  https://raw.githubusercontent.com/teohhanhui/whatsapp-web-dark-toggle/master/user.js
+// @supportURL   https://github.com/teohhanhui/whatsapp-web-dark-toggle/issues
+// @match        https://web.whatsapp.com/*
+// @run-at       document-body
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  document.querySelector('body').classList.add('dark');
+})();
