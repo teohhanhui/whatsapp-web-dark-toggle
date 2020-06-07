c096a4286d9e7b3ff2a976874d8ff90f3e2256a0
Teoh Han Hui <teohhanhui@gmail.com>
2020-06-07 20:50:45 +0200

    Add "dark" class to "html" element too
    The body seems to get replaced, or its class list is being reset

--- user.js
+++ user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WhatsApp Web dark toggle
 // @namespace    https://github.com/teohhanhui/whatsapp-web-dark-toggle
-// @version      0.1
+// @version      0.2
 // @author       Teoh Han Hui <teohhanhui@gmail.com>
 // @description  Force toggle WIP dark theme on WhatsApp Web
 // @updateURL    https://raw.githubusercontent.com/teohhanhui/whatsapp-web-dark-toggle/master/user.js
@@ -15,5 +15,6 @@
 (function() {
   'use strict';
 
+  document.querySelector('html').classList.add('dark');
   document.querySelector('body').classList.add('dark');
 })();
